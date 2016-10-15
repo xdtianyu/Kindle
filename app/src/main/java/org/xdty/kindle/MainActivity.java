4cb2027c0ae45802cb40a1d758d1962823d5d11e
tianyu <xdtianyu@gmail.com>
2016-10-15 22:16:09 +0800

    update layout

--- app/src/main/java/org/xdty/kindle/MainActivity.java
+++ app/src/main/java/org/xdty/kindle/MainActivity.java
@@ -1,9 +1,17 @@
 package org.xdty.kindle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import org.xdty.kindle.application.Application;
 import org.xdty.kindle.data.BookDataSource;
@@ -16,12 +24,17 @@ import javax.inject.Inject;
 
 import rx.functions.Action1;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     BookDataSource mBookDataSource;
 
     private RecyclerView mRecyclerView;
+    private Toolbar mToolbar;
+
+    private DrawerLayout mDrawer;
+
     private BooksAdapter mBooksAdapter;
 
     @Override
@@ -32,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
         Application.getAppComponent().inject(this);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(mToolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mBooksAdapter = new BooksAdapter();
 
@@ -44,5 +70,57 @@ public class MainActivity extends AppCompatActivity {
                 mBooksAdapter.refresh(books);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        mDrawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
