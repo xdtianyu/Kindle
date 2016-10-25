84bd982045b909b99feb202cc110fca07513a7cd
tianyu <xdtianyu@gmail.com>
2016-10-25 13:36:40 +0800

    add node support

--- app/src/main/java/org/xdty/kindle/module/Book.java
+++ app/src/main/java/org/xdty/kindle/module/Book.java
@@ -1,5 +1,7 @@
 package org.xdty.kindle.module;
 
+import java.util.List;
+
 public class Book {
 
     private String title;
@@ -10,6 +12,168 @@ public class Book {
     private double score;
     private String url;
     private String minDay;
+    private String item_id;
+    private String pages;
+    private String publisher;
+    private String brand;
+    private String asin;
+    private String binding;
+    private String edition;
+    private String editorial_review;
+    private String isbn;
+    private String large_image_url;
+    private String medium_image_url;
+    private String small_image_url;
+    private String region;
+    private String release_date;
+    private String publication_date;
+    private String sales_rank;
+    private String languages;
+    private List<Node> nodes;
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getAsin() {
+        return asin;
+    }
+
+    public void setAsin(String asin) {
+        this.asin = asin;
+    }
+
+    public String getBinding() {
+        return binding;
+    }
+
+    public void setBinding(String binding) {
+        this.binding = binding;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getEditorial_review() {
+        return editorial_review;
+    }
+
+    public void setEditorial_review(String editorial_review) {
+        this.editorial_review = editorial_review;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getLarge_image_url() {
+        return large_image_url;
+    }
+
+    public void setLarge_image_url(String large_image_url) {
+        this.large_image_url = large_image_url;
+    }
+
+    public String getMedium_image_url() {
+        return medium_image_url;
+    }
+
+    public void setMedium_image_url(String medium_image_url) {
+        this.medium_image_url = medium_image_url;
+    }
+
+    public String getSmall_image_url() {
+        return small_image_url;
+    }
+
+    public void setSmall_image_url(String small_image_url) {
+        this.small_image_url = small_image_url;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getPublication_date() {
+        return publication_date;
+    }
+
+    public void setPublication_date(String publication_date) {
+        this.publication_date = publication_date;
+    }
+
+    public String getSales_rank() {
+        return sales_rank;
+    }
+
+    public void setSales_rank(String sales_rank) {
+        this.sales_rank = sales_rank;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
 
     public String getTitle() {
         return title;
